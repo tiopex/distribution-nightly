8a85a62cc1ba6575985a74dde3bfe2a64bf61651
Rudi Heitbaum <rudi@heitbaum.com>
2024-09-25 11:34:56 +0000

    libarchive: update to 3.7.7

--- packages/compress/libarchive/package.mk
+++ packages/compress/libarchive/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libarchive"
-PKG_VERSION="3.7.5"
-PKG_SHA256="ca74ff8f99dd40ab8a8274424d10a12a7ec3f4428dd35aee9fdda8bdb861b570"
+PKG_VERSION="3.7.7"
+PKG_SHA256="879acd83c3399c7caaee73fe5f7418e06087ab2aaf40af3e99b9e29beb29faee"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.libarchive.org"
 PKG_URL="https://www.libarchive.org/downloads/${PKG_NAME}-${PKG_VERSION}.tar.xz"
