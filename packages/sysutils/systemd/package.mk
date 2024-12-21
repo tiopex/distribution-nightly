b3f7a8667d8ba5a1e9851056595bf0d11632f0a1
Christian Hewitt <christianshewitt@gmail.com>
2024-12-21 13:48:42 +0000

    systemd: support console login when LOCAL_LOGIN is defined
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -134,14 +134,16 @@ post_makeinstall_target() {
   safe_remove ${INSTALL}/usr/lib/udev/rules.d/71-seat.rules
   safe_remove ${INSTALL}/usr/lib/udev/rules.d/73-seat-late.rules
 
-  # remove getty units, we dont want a console
-  safe_remove ${INSTALL}/usr/lib/systemd/system/autovt@.service
-  safe_remove ${INSTALL}/usr/lib/systemd/system/console-getty.service
-  safe_remove ${INSTALL}/usr/lib/systemd/system/container-getty@.service
-  safe_remove ${INSTALL}/usr/lib/systemd/system/getty.target
-  safe_remove ${INSTALL}/usr/lib/systemd/system/getty@.service
-  safe_remove ${INSTALL}/usr/lib/systemd/system/serial-getty@.service
-  safe_remove ${INSTALL}/usr/lib/systemd/system/*.target.wants/getty.target
+  if [ "${LOCAL_LOGIN}" = "no" ]; then
+    # remove getty units, we dont want a console
+    safe_remove ${INSTALL}/usr/lib/systemd/system/autovt@.service
+    safe_remove ${INSTALL}/usr/lib/systemd/system/console-getty.service
+    safe_remove ${INSTALL}/usr/lib/systemd/system/container-getty@.service
+    safe_remove ${INSTALL}/usr/lib/systemd/system/getty.target
+    safe_remove ${INSTALL}/usr/lib/systemd/system/getty@.service
+    safe_remove ${INSTALL}/usr/lib/systemd/system/serial-getty@.service
+    safe_remove ${INSTALL}/usr/lib/systemd/system/*.target.wants/getty.target
+  fi
 
   # remove other notused or nonsense stuff (our /etc is ro)
   safe_remove ${INSTALL}/usr/lib/systemd/systemd-update-done
@@ -206,6 +208,10 @@ post_makeinstall_target() {
   sed -e "s,^.*SystemMaxUse=.*$,SystemMaxUse=10M,g" -i ${INSTALL}/etc/systemd/journald.conf
 
   # tune logind.conf
+  if [ "${LOCAL_LOGIN}" = "yes" ]; then
+    sed -e "s,^.*NAutoVTs=.*$,NAutoVTs=2,g" -i ${INSTALL}/etc/systemd/logind.conf
+    sed -e "s,^.*ReserveVT=.*$,ReserveVT=6,g" -i ${INSTALL}/etc/systemd/logind.conf
+  fi
   sed -e "s,^.*HandleLidSwitch=.*$,HandleLidSwitch=ignore,g" -i ${INSTALL}/etc/systemd/logind.conf
   if [ "${DISPLAYSERVER}" = "no" ]; then
     sed -e "s,^.*HandlePowerKey=.*$,HandlePowerKey=poweroff,g" -i ${INSTALL}/etc/systemd/logind.conf
@@ -308,4 +314,7 @@ post_install() {
   enable_service systemd-timesyncd.service
   enable_service systemd-timesyncd-setup.service
   enable_service systemd-userdbd.socket
+  if [ "${LOCAL_LOGIN}" = "yes" ]; then
+    enable_service getty@tty0.service
+  fi
 }
