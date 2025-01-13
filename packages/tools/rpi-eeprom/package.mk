cc42143d092d74915d34227a204f6c7e1df60a72
Matthias Reichl <hias@horus.com>
2025-01-13 19:53:04 +0100

    rpi-eeprom: update to 6a7ca68
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/rpi-eeprom/package.mk
+++ packages/tools/rpi-eeprom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rpi-eeprom"
-PKG_VERSION="54d9c333a9d39941b4fc881275f433821c7b5cde"
-PKG_SHA256="4424884c26bea47fe307b2de0087b1d7026da8f66d7ca20beb6e988da02670b1"
+PKG_VERSION="6a7ca681cb932eaaf6cdaf09e3edbcc89b53fa84"
+PKG_SHA256="9f9dad707f2e2c600cf7e5bf2a424d0fa31ee77eeddd9cb71d1bd5f21d5b22f7"
 PKG_LICENSE="BSD-3/custom"
 PKG_SITE="https://github.com/raspberrypi/rpi-eeprom"
 PKG_URL="https://github.com/raspberrypi/rpi-eeprom/archive/${PKG_VERSION}.tar.gz"
