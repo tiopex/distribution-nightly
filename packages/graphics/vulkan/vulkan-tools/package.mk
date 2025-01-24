f33cc8b54fa12a03f1f1934d0dacaeddc41f60ac
Rudi Heitbaum <rudi@heitbaum.com>
2025-01-24 23:15:03 +0000

    vulkan-tools: update to 1.4.306

--- packages/graphics/vulkan/vulkan-tools/package.mk
+++ packages/graphics/vulkan/vulkan-tools/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vulkan-tools"
-PKG_VERSION="1.4.305"
-PKG_SHA256="55c25e50172428f36e63cc9644c25fcda2a558b31158283f2cbba685f0b94572"
+PKG_VERSION="1.4.306"
+PKG_SHA256="8e3c3df74fa8e6408cbb772d20012c3fdf0bf1b3e363e18e909b6c99d5b489a2"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Tools"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/v${PKG_VERSION}.tar.gz"
