705006794faa0f8d4151323071b6434850a5e812
Matthias Reichl <hias@horus.com>
2025-01-27 20:56:12 +0100

    rpi-eeprom: update to 0190dbc
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/rpi-eeprom/package.mk
+++ packages/tools/rpi-eeprom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rpi-eeprom"
-PKG_VERSION="b67b21ddda8b6468090fcdc5034bb075344a8903"
-PKG_SHA256="8105d2d90fde2d109bf5c2fc45276e6570cb37aedd23b3fd36ba2f3e4249daf5"
+PKG_VERSION="0190dbc122fe0adeed591375fb4e80fbfe6aae15"
+PKG_SHA256="09e6043d72c3dc388376f7bb5e2bc9c29d529e68c48b9018c0c3a36262002db5"
 PKG_LICENSE="BSD-3/custom"
 PKG_SITE="https://github.com/raspberrypi/rpi-eeprom"
 PKG_URL="https://github.com/raspberrypi/rpi-eeprom/archive/${PKG_VERSION}.tar.gz"
