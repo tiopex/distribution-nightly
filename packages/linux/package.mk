61b1d89a43312359096dc769ae6d0cbed645b7e4
Rudi Heitbaum <rudi@heitbaum.com>
2025-03-23 04:45:34 +0000

    linux: update to 6.12.20

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -30,8 +30,8 @@ case "${LINUX}" in
     PKG_PATCH_DIRS="raspberrypi rtlwifi/6.13 rtlwifi/6.14 rtlwifi/after-6.14"
     ;;
   *)
-    PKG_VERSION="6.12.17"
-    PKG_SHA256="5c205cd34f80974e4973e321cb008f5f6895a8aa8c2577f06a9448cd77de63b3"
+    PKG_VERSION="6.12.20"
+    PKG_SHA256="230e89b07b0ab82e74f07ecc1bee3105dca81d0ef4a97f900929c407249b6ac7"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default rtlwifi/6.13 rtlwifi/6.14 rtlwifi/after-6.14"
     ;;
