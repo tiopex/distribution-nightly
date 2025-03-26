9aa2fbbae4c800e724082d33c13be3219b46477c
Rudi Heitbaum <rudi@heitbaum.com>
2025-03-26 08:04:06 +0000

    fakeroot: update to 1.37.1.1

--- packages/devel/fakeroot/package.mk
+++ packages/devel/fakeroot/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fakeroot"
-PKG_VERSION="1.37.1"
-PKG_SHA256="1af13a9745d5c2b5ef691623c9a435417fb5549d549f17633723463c34095162"
+PKG_VERSION="1.37.1.1"
+PKG_SHA256="86b0b75bf319ca42e525c098675b6ed10a06b76e69ec9ccf20ef5e03883b3a14"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://tracker.debian.org/pkg/fakeroot"
 PKG_URL="http://ftp.debian.org/debian/pool/main/f/fakeroot/${PKG_NAME}_${PKG_VERSION}.orig.tar.gz"
