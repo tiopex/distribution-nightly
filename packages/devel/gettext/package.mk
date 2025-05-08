eb190c37f7f06ac303cc6abe64d92a747eb21504
Rudi Heitbaum <rudi@heitbaum.com>
2025-05-08 02:25:33 +0000

    gettext: update to 0.25
    news: https://savannah.gnu.org/news/?id=10769

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gettext"
-PKG_VERSION="0.24.1"
-PKG_SHA256="6164ec7aa61653ac9cdfb41d5c2344563b21f707da1562712e48715f1d2052a6"
+PKG_VERSION="0.25"
+PKG_SHA256="05240b29f5b0f422e5a4ef8e9b5f76d8fa059cc057693d2723cdb76f36a88ab0"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.gnu.org/s/gettext/"
 PKG_URL="https://ftp.gnu.org/pub/gnu/gettext/${PKG_NAME}-${PKG_VERSION}.tar.xz"
