c1180c471a922b189fe0327fc879c2c63b7318bc
Christian Hewitt <christianshewitt@gmail.com>
2025-06-09 08:55:40 +0000

    linux: update Amlogic to Linux 6.16.y kernel and patches
    Signed-off-by: Christian Hewitt <christianshewitt@gmail.com>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -16,8 +16,8 @@ PKG_PATCH_DIRS="${LINUX}"
 
 case "${LINUX}" in
   amlogic)
-    PKG_VERSION="3ef49626da6dd67013fc2cf0a4e4c9e158bb59f7" # 6.15.1
-    PKG_SHA256="dd0c9b097906d7cbcd1281c0fbc489ffdd803495bffd2d159c5e994213362468"
+    PKG_VERSION="86731a2a651e58953fc949573895f2fa6d456841" # 6.16-rc3
+    PKG_SHA256="008b00968a8bfc0627580b82a2d30c7304336a4f92a58e80cdbc2d4723e01840"
     PKG_URL="https://github.com/torvalds/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS="default"
