dd413ef8a51321664ab1195d80981a4fd25fbaaf
Rudi Heitbaum <rudi@heitbaum.com>
2025-06-09 00:34:53 +0000

    assimp: update to 6.0.2

--- packages/graphics/assimp/package.mk
+++ packages/graphics/assimp/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="assimp"
-PKG_VERSION="6.0.1"
-PKG_SHA256="0c6ec0e601cab4700019c1e60b5cd332cc6355e63e59c11344693623c08a7d38"
+PKG_VERSION="6.0.2"
+PKG_SHA256="d1822d9a19c9205d6e8bc533bf897174ddb360ce504680f294170cc1d6319751"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/assimp/assimp"
 PKG_URL="https://github.com/assimp/assimp/archive/v${PKG_VERSION}.tar.gz"
