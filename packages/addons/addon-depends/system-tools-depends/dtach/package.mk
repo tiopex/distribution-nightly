a747cc86ce76c62753c635c945da847bbfe1a04f
Rudi Heitbaum <rudi@heitbaum.com>
2025-06-18 07:38:57 +0000

    dtach: update to githash 5e38653
    - includes now upstreamed patches

--- packages/addons/addon-depends/system-tools-depends/dtach/package.mk
+++ packages/addons/addon-depends/system-tools-depends/dtach/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dtach"
-PKG_VERSION="0.9"
-PKG_SHA256="5f7e8c835ee49a9e6dcf89f4e8ccbe724b061c0fc8565b504dd8b3e67ab79f82"
+PKG_VERSION="5e3865328e4609f74bd10cb4575e429b8d1b3aa4"
+PKG_SHA256="69c6c3da78c530ec74655d6beba0d2d3e16a23ab35b3022d8af691848063cc1e"
 PKG_LICENSE="GPL"
 PKG_SITE="http://dtach.sourceforge.net"
-PKG_URL="https://github.com/crigler/dtach/archive/v${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/crigler/dtach/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A program that emulates the detach feature of screen."
 PKG_BUILD_FLAGS="-sysroot -cfg-libs"
