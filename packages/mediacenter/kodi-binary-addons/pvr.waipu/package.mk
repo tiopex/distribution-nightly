83590315020f6c635da46bf5a6345671fb425104
Rudi Heitbaum <rudi@heitbaum.com>
2025-06-20 12:02:52 +0000

    addons: bump kodi-binary-addons
    Signed-off-by: Rudi Heitbaum <rudi@heitbaum.com>

--- packages/mediacenter/kodi-binary-addons/pvr.waipu/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.waipu/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="pvr.waipu"
 PKG_VERSION="22.5.1-Piers"
 PKG_SHA256="ba5088c86f17ed8b9d1ac571ca24beedb6aec954dfafd2a3b96ad4b2a295159c"
-PKG_REV="1"
+PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/flubshi/pvr.waipu"
