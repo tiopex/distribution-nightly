0b0dcd4a2a4ffa6b410af01ee01eace652df7fb7
Rudi Heitbaum <rudi@heitbaum.com>
2025-06-26 23:39:59 +0000

    cargo-snapshot: update to 1.88.0

--- packages/rust/cargo-snapshot/package.mk
+++ packages/rust/cargo-snapshot/package.mk
@@ -10,15 +10,15 @@ PKG_TOOLCHAIN="manual"
 
 case "${MACHINE_HARDWARE_NAME}" in
   "aarch64")
-    PKG_SHA256="51e237e7f383840a404a5be721491a8ca4671bf9c14e62566ecadccfcc6e4291"
+    PKG_SHA256="5aa43865f2002914ce4fca8916b4403bfca62f17e779ad368f6a17553296a58b"
     PKG_URL="https://static.rust-lang.org/dist/cargo-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
     ;;
   "arm")
-    PKG_SHA256="661376af716de8d6abf943891bc0d8ff6b1733d97ee7da9be92820b8e11d20d1"
+    PKG_SHA256="ad1b3737e7ac3a736a95f37ddd9a5d2d19f40dfc89197d544e9ca593c09f4dc7"
     PKG_URL="https://static.rust-lang.org/dist/cargo-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnueabihf.tar.xz"
     ;;
   "x86_64")
-    PKG_SHA256="469d5dc479835adadd728bc3587f8abf1941b3dd71f9865abd3e0783ae662555"
+    PKG_SHA256="856962610ee821648cee32e3d6abac667af7bb7ea6ec6f3d184cc31e66044f6b"
     PKG_URL="https://static.rust-lang.org/dist/cargo-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
     ;;
 esac
