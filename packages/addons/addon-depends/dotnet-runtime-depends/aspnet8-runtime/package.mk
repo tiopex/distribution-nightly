a524da63d33e4bba3beffb0aaa4a8a4c12c873e2
Rudi Heitbaum <rudi@heitbaum.com>
2025-07-09 11:12:43 +0000

    aspnet8-runtime: update to 8.0.18

--- packages/addons/addon-depends/dotnet-runtime-depends/aspnet8-runtime/package.mk
+++ packages/addons/addon-depends/dotnet-runtime-depends/aspnet8-runtime/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="aspnet8-runtime"
-PKG_VERSION="8.0.17"
+PKG_VERSION="8.0.18"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_DEPENDS_TARGET="toolchain"
@@ -11,16 +11,16 @@ PKG_TOOLCHAIN="manual"
 
 case "${ARCH}" in
   "aarch64")
-    PKG_SHA256="a8e2afbfadd9a13a80c710bc0a37ae33eabd701ca76985e77b685b96bfcfcc97"
-    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.17/aspnetcore-runtime-8.0.17-linux-arm64.tar.gz"
+    PKG_SHA256="74b20f12c7e48f47d90e150a646f05bf1b6db34659f28d34ac25fa7be35dd66f"
+    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.18/aspnetcore-runtime-8.0.18-linux-arm64.tar.gz"
     ;;
   "arm")
-    PKG_SHA256="98d7dec7cb253cd7c3863a2426fc2b24803dd48e27c18c630c8bb6ba550f183b"
-    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.17/aspnetcore-runtime-8.0.17-linux-arm.tar.gz"
+    PKG_SHA256="28899322ab8af69708202f7797abe747a17566da84e6fd3747a49af95cc19bff"
+    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.18/aspnetcore-runtime-8.0.18-linux-arm.tar.gz"
     ;;
   "x86_64")
-    PKG_SHA256="f8944510379f8e1d5a6e7681692225f509f5df348157205d4dbe5b740b85dc98"
-    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.17/aspnetcore-runtime-8.0.17-linux-x64.tar.gz"
+    PKG_SHA256="3902cd2a0742e6f59b72e031bc46a6f39cdf070ec2ee20174ebd373dd496a652"
+    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.18/aspnetcore-runtime-8.0.18-linux-x64.tar.gz"
     ;;
 esac
 PKG_SOURCE_NAME="aspnetcore-runtime_${PKG_VERSION}_${ARCH}.tar.gz"
