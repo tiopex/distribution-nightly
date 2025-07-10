19828ebbd7284b08cbfaa2244efc3d759f8756ad
Rudi Heitbaum <rudi@heitbaum.com>
2025-07-10 13:32:31 +0000

    commons-lang3: update to 3.18.0

--- packages/devel/commons-lang3/package.mk
+++ packages/devel/commons-lang3/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="commons-lang3"
-PKG_VERSION="3.17.0"
-PKG_SHA256="08b93712bed7f48725d93c44d70c71e7e661af390f22f0f3e6ba61e3af3cea36"
+PKG_VERSION="3.18.0"
+PKG_SHA256="add5f9cd656437515d19c8af46058322f9bbc337591a811027f706df84ee5b14"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://commons.apache.org/proper/commons-lang/"
 PKG_URL="https://archive.apache.org/dist/commons/lang/binaries/commons-lang3-${PKG_VERSION}-bin.tar.gz"
